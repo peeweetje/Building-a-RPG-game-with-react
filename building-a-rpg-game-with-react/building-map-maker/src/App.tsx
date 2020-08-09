f7dbe2d9f7adb324bca7a12f52eb3bd59c829cb7
peeweetje <petravos70@gmail.com>
2020-08-09 16:17:12 +0200

    Refactor: add tile-pallet component to app. Clean-up code.

--- building-a-rpg-game-with-react/building-map-maker/src/App.tsx
+++ building-a-rpg-game-with-react/building-map-maker/src/App.tsx
@@ -1,78 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styles from 'styled-components';
+import { useDraggable } from './components/custom-hooks/use-draggable';
+import TilePallet from './components/tile-pallet/tile-pallet';
 
-type styledContainerProps = {
+type styledAppProps = {
   width?: number;
   height?: number;
 };
 
-type styledToolBarProps = {
-  position: { x: number; y: number };
-};
-
 const App = () => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const handle = document.getElementById('handle');
-    handle?.addEventListener('mousedown', function (e) {
-      e.preventDefault();
-      handle.style.pointerEvents = 'none';
-
-      document.body.addEventListener('mousemove', move);
-      document.body.addEventListener('mouseup', () => {
-        document.body.removeEventListener('mousemove', move);
-        handle.style.pointerEvents = 'initial';
-      });
-    });
-    return () => {
-      document.body.removeEventListener('mousedown', move);
-      document.body.removeEventListener('mouseup', move);
-      document.body.removeEventListener('mousemove', move);
-    };
-  }, []);
-
-  const move = (e: any) => {
-    const pos = {
-      x: e.clientX,
-      y: e.clientY,
-    };
-    setPosition(pos);
-  };
-
+  const { position } = useDraggable('handle');
   return (
     <ContainerDiv width={window.innerWidth} height={window.innerHeight}>
-      <ToolbarDiv position={position}>
-        <img id='handle' src='/images/drag-handle.png' alt='drag-handle' />
-      </ToolbarDiv>
+      <TilePallet position={position} size={{ height: 288, width: 640 }} />
     </ContainerDiv>
   );
 };
 
 export default App;
 
-const ContainerDiv = styles.div.attrs<styledContainerProps>((props) => ({
+const ContainerDiv = styles.div.attrs<styledAppProps>((props) => ({
   style: {
-    width: `${props.width}px`,
-    height: `${props.height}px`,
+    width: props.width,
+    height: props.height,
   },
-}))<styledContainerProps>`
+}))<styledAppProps>`
   position: relative;
   background-color: grey;
   overflow: hidden;
   border: 1px solid black;
-`;
-
-const ToolbarDiv = styles.div.attrs<styledToolBarProps>((props) => ({
-  style: {
-    top: props.position.y,
-    left: props.position.x,
-  },
-}))<styledToolBarProps>`
-position: absolute;
-border: 1px solid black;
-z-index: 1;
-width: 200;
-height: 200;
-background-color: white;
 `;
