869a23eab86ba5369916595cce1c2aaa3dc5c61a
peeweetje <petravos70@gmail.com>
2020-08-11 21:10:25 +0200

    Adding the map builder to the app.

--- building-a-rpg-game-with-react/building-map-maker/src/App.tsx
+++ building-a-rpg-game-with-react/building-map-maker/src/App.tsx
@@ -1,18 +1,53 @@
-import React from 'react';
-import styles from 'styled-components';
-import { useDraggable } from './components/custom-hooks/use-draggable';
-import TilePallet from './components/tile-pallet/tile-pallet';
+import React, { useEffect, useState } from "react";
+import styles from "styled-components";
+import { useDraggable } from "./components/custom-hooks/use-draggable";
+import TilePallet from "./components/tile-pallet/tile-pallet";
+import MapBuilder from "./components/map/map";
 
 type styledAppProps = {
-  width?: number;
-  height?: number;
+  width: number;
+  height: number;
 };
 
+interface IStateObject {
+  x: number;
+  y: number;
+  id: number;
+  v: { x: number; y: number };
+}
+
 const App = () => {
-  const { position } = useDraggable('handle');
+  const [tileSet, setTileSet] = useState("sprites/rpg-nature-tileset/spring");
+  const [tiles, setTiles] = useState<IStateObject[][]>([]);
+  const [mapSize, setMapSize] = useState({ width: 800, height: 600 });
+  const { position } = useDraggable("handle");
+
+  useEffect(() => {
+    const _tiles = [];
+    let id = 0;
+
+    for (let y = 0; y < mapSize.height; y = y + 32) {
+      const row = [];
+      for (let x = 0; x < mapSize.width; x = x + 32) {
+        row.push({ x, y, id: id++, v: { x: -32, y: -32 } });
+      }
+      _tiles.push(row);
+    }
+    setTiles(_tiles);
+  }, [mapSize.height, mapSize.width]);
+
   return (
     <ContainerDiv width={window.innerWidth} height={window.innerHeight}>
-      <TilePallet position={position} size={{ height: 288, width: 640 }} />
+      <TilePallet
+        tileSet={tileSet}
+        position={position}
+        size={{ height: 288, width: 640 }}
+      />
+      <MapBuilder
+        tile={tiles}
+        tiles={tiles}
+        size={{ height: 576, width: 640 }}
+      />
     </ContainerDiv>
   );
 };
